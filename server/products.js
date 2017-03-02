0002460244821bbe1b497c9cf2d883b4d6568d46
Christine Dillon <christine.dillon@gmail.com>
2017-03-02 18:37:47 -0500

    Completed functionality to remove items from the cart in both the state and databse.

--- server/products.js
+++ server/products.js
@@ -26,7 +26,7 @@ router.get('/', function( req, res, next) {
 router.get('/:id', function(req, res, next) {
     Products.findById(req.params.id)
     .then(function(product){
-      res.send(product);
+      res.send(product)
     })
     .catch(next)
 })
