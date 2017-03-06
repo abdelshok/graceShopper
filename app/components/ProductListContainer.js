96d7d4f71d47e12c12950c70b0520de7347f9eb2
abdelshok <aas698@nyu.edu>
2017-03-06 17:59:17 -0500

    fixed indenting

--- app/components/ProductListContainer.js
+++ app/components/ProductListContainer.js
@@ -13,12 +13,12 @@ const mapStateToProps = ({products}) => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-		onToClick() {
+	onToClick() {
 		throw Error('to do')
-		},
-		productSearch(title) {
+	},
+	productSearch(title) {
 		dispatch(loadSelectedProduct(title))
-		}
+	}
 })
 
 class ProductListContainer extends Component {
@@ -29,62 +29,61 @@ class ProductListContainer extends Component {
 			inputValue: '',
 			search: 'Artist Name'
 		}
-	    this.handleChange = this.handleChange.bind(this)
-	    this.onClick = this.onClick.bind(this)
-}
+		this.handleChange = this.handleChange.bind(this)
+		this.onClick = this.onClick.bind(this)
+	}
 
 
 	handleChange (evt) {
-	    this.setState({
-	      inputValue: evt.target.value
-	    })
+		this.setState({
+			inputValue: evt.target.value
+		})
 	}
 
 	onClick (filter) {
 		this.setState({
-		inputValue: '',
-		search: filter
+			inputValue: '',
+			search: filter
 		})
 	}
 
 	render() {
 		const styles = { block: { maxWidth: 250 }, checkbox: { marginBottom: 16 } }
 		const inputValue = this.state.inputValue
-		const displayStyle = { display: 'inline-block'}	
+		const displayStyle = { display: 'inline-block'}
 
 		let filteredProducts = this.props.products.filter(product => product.artistName.toLowerCase().match(inputValue))
-			if (this.state.search === 'Artist Name'){
-				filteredProducts = this.props.products.filter(product => product.artistName.toLowerCase().match(inputValue))
-			} else if (this.state.search === 'Title'){
-				filteredProducts = this.props.products.filter(product => product.title.toLowerCase().match(inputValue))
-			} else if (this.state.search === 'Color'){
-				filteredProducts = this.props.products.filter(product => {
-					if (inputValue !== ''){
-						console.log(product.tags.map(tags => tags.toLowerCase()), 'these are tags')
-						return product.tags.map(tags => tags.toLowerCase()).includes(inputValue)				
-					}
-					else {
-						return filteredProducts = this.props.products.filter(product => product.title.toLowerCase().match(inputValue))
-					}
-				})
-			} else if (this.state.search === 'Medium'){
-				filteredProducts = this.props.products.filter(product =>  product.medium.toLowerCase().match(inputValue))
-			}
+		if (this.state.search === 'Artist Name'){
+			filteredProducts = this.props.products.filter(product => product.artistName.toLowerCase().match(inputValue))
+		} else if (this.state.search === 'Title'){
+			filteredProducts = this.props.products.filter(product => product.title.toLowerCase().match(inputValue))
+		} else if (this.state.search === 'Color'){
+			filteredProducts = this.props.products.filter(product => {
+				if (inputValue !== ''){
+					console.log(product.tags.map(tags => tags.toLowerCase()), 'these are tags')
+					return product.tags.map(tags => tags.toLowerCase()).includes(inputValue)				
+				}
+				else {
+					return filteredProducts = this.props.products.filter(product => product.title.toLowerCase().match(inputValue))
+				}
+			})
+		} else if (this.state.search === 'Medium'){
+			filteredProducts = this.props.products.filter(product =>  product.medium.toLowerCase().match(inputValue))
+		}
 		return (
 			<div>
-				<h1> Search The Collection</h1>
+			<h1> Search The Collection</h1>
 
-				<FilterInputComponent handleChange={this.handleChange} inputValue={inputValue} searchTerm={this.state.search} />
-				<div style={displayStyle} >
-					<RadioButton label="Artist Name" style={displayStyle} onClick={() => this.onClick('Artist Name')} />
-					<RadioButton label="Title" style={displayStyle} onClick={() => this.onClick('Title')} />
-					<RadioButton label="Color" style={displayStyle} onClick={() => this.onClick('Color')} />
-					<RadioButton label="Medium" style={displayStyle} onClick={() => this.onClick('Medium')} />
-				</div>
-				<ProductListComponent products={filteredProducts} />
+			<FilterInputComponent handleChange={this.handleChange} inputValue={inputValue} searchTerm={this.state.search} />
+			<div style={displayStyle} >
+			<RadioButton label="Artist Name" style={displayStyle} onClick={() => this.onClick('Artist Name')} />
+			<RadioButton label="Title" style={displayStyle} onClick={() => this.onClick('Title')} />
+			<RadioButton label="Color" style={displayStyle} onClick={() => this.onClick('Color')} />
+			<RadioButton label="Medium" style={displayStyle} onClick={() => this.onClick('Medium')} />
 			</div>
-
-		)
+			<ProductListComponent products={filteredProducts} />
+			</div>
+			)
 	}
 }
 
